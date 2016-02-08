9487852d64813e8657378ea9507166ccba6460b2
Ilmir Taychinov <riml@railgun-games.com>
2016-02-08 17:23:47 -0500

    Gulp and requried utils added

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+//added required modules to the project
+var gulp =require('gulp');
+var gutile = require('gulp-util');
+
+//example task
+gulp.task('example',function(){
+    gutile.log("gulp example started....");
+});
