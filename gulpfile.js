ed525d95bea6d00c986d9e82b43e84dae02a6369
Ilmir Taychinov <riml@railgun-games.com>
2016-02-08 17:44:14 -0500

    added transpile  test to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,35 @@
 //added required modules to the project
-var gulp =require('gulp');
-var gutile = require('gulp-util');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var debug = require('gulp-debug');
+var inject = require('gulp-inject');
+var tsc = require('gulp-typescript');
+var sourcemaps = require('gulp-sourcemaps');
+var tscProject = tsc.createProject('tsconfig.json');
+var connect = require('gulp-connect');
+var open = require('gulp-open');
 
-//example task
-gulp.task('example',function(){
-    gutile.log("gulp example started....");
+// Declare file sources
+var TypeScriptSources = [
+    './Scripts/**/*.ts',
+    './typings/**/*.ts'];
+    
+var HTMLSources = ['./**/*.html'];
+var CSSSources = ['./Content/**/*.css'];
+// This task Transpiles TypeScript to JavaScript
+gulp.task('transpile', function () {
+    gutil.log("transpiling...");
+
+    var tsResult = gulp.src(TypeScriptSources)
+        .pipe(sourcemaps.init())
+        .pipe(tsc(tscProject))
+        .on('error', gutil.log);
+
+    tsResult.dts.pipe(gulp.dest('./Scripts/'));
+    return tsResult.js
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./Scripts/'))
+        .on('error', gutil.log);
+        //.pipe(connect.reload());
+
 });
--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -11,3 +11,7 @@
 /// <reference path="gulp-util/gulp-util.d.ts" />
 /// <reference path="through2/through2.d.ts" />
 /// <reference path="vinyl/vinyl.d.ts" />
+/// <reference path="gulp-debug/gulp-debug.d.ts" />
+/// <reference path="gulp-inject/gulp-inject.d.ts" />
+/// <reference path="gulp-typescript/gulp-typescript.d.ts" />
+/// <reference path="gulp-sourcemaps/gulp-sourcemaps.d.ts" />
